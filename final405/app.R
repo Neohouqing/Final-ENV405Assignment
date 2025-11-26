1a2a55d89bc4989d32fa9709c4f4ff402bae62b5
叶厚清 <neo@xiehouqingdeMacBook-Pro.local>
2025-11-27 02:08:02 +0800

    Add basic Shiny app to preview wind data

--- final405/app.R
+++ final405/app.R
@@ -1,12 +1,20 @@
-
 library(shiny)
+library(readr)
+
+wind <- read_csv("../data/wind.csv")
 
 ui <- fluidPage(
-  titlePanel("ENV405 Final Shiny App")
+  titlePanel("ENV405 - Wind data explorer (preview only)"),
+  mainPanel(
+    h4("First 20 rows of wind data"),
+    tableOutput("preview")
+  )
 )
 
 server <- function(input, output, session) {
+  output$preview <- renderTable({
+    head(wind, 20)
+  })
 }
 
-shinyApp(ui, server)
-
+shinyApp(ui = ui, server = server)
